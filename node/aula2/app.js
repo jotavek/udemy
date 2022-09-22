0522bd59a3611d2d2ff649cdfa04813dbaedb490
João Victor Galvão de Oliveira <jotavedev@gmail.com>
2022-09-22 17:33:27 -0300

    node

---
+++ node/aula2/app.js
@@ -0,0 +1,8 @@
+// Para navegar em pastas:
+// ./ Permanece na pasta em que o arquivo está localizado.
+// ../ Volta uma pasta.
+
+const Cachorro = require('./mod');
+
+const cachorrinho = new Cachorro('Max');
+cachorrinho.latir();
