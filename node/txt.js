2d27af01d9be90e0b90c1b7de334439bb58632c8
João Victor Galvão de Oliveira <jotavedev@gmail.com>
2022-09-22 17:52:39 -0300

    node

---
+++ node/txt.js
@@ -0,0 +1,12 @@
+/*
+Comandos do npm;
+install init para iniciar
+install pra instalar 
+-E para uma versao específica
+--save-dev move para Dev dependencies 
+--save-prod volta pra produção (padrao)
+-ls lista os pacotes que temos
+upgrade atualiza
+outdated mostra os pacotes desatualizados
+desinsatala com uninstall
+*/
