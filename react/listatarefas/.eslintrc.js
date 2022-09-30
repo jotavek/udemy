5651db97031c87d95ffbdb7ae2712c4829aa8100
João Victor Galvão de Oliveira <jotavedev@gmail.com>
2022-09-30 13:34:56 -0300

    Lista de tarefas

--- react/listatarefas/.eslintrc.js
+++ react/listatarefas/.eslintrc.js
@@ -2,21 +2,34 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
+    node: true,
   },
   extends: [
+    'eslint:recommended',
     'plugin:react/recommended',
-    'airbnb',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
-  overrides: [
-  ],
-  parser:'@babel-eslint',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@babel/eslint-parser',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react', 'prettier', 'react-hooks'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
+    'react/react-in-jsx-scope': 'off',
   },
 };
---
+++ react/listatarefas/src/components/Main.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './Main.css';
+
+// Form
+import { GiCrosshair } from 'react-icons/gi';
+
+export default class Main extends Component {
+  state = {
+    novaTarefa: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  };
+
+  render() {
+    const { novaTarefa } = this.state;
+    return (
+      <div className="main">
+        <h1>Lista de tarefas</h1>
+
+        <form action="#" className="form">
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={novaTarefa}
+          ></input>
+          <button type="submit">
+            <GiCrosshair />
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
