7bc192bc39d73d55e59d5dd96d2c129c14189a37
João Victor Galvão de Oliveira <jotavedev@gmail.com>
2022-12-01 19:11:44 -0300

    js

--- aula for of/main.js
+++ aula for of/main.js
@@ -1,9 +1,10 @@
 //For of - Retorna o valor em si (iteráveis, arrays ou strings)
 
-const nome = ['Joao Victor', 'Paulo', 'Fernanda']
+const nome = ['Joao Victor', 'Paulo', 'Fernanda', 'Pedro']
 
 for (let valor of nome) {
     console.log(valor);
 }
+
 
 
